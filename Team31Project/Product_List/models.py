bfcfbb8eae8ed261667ea6720be1593fdd1d906f
Harvey <240326227@aston.ac.uk>
2025-10-21 14:41:59 +0100

    Updated Product_List models to import "Stock" db

--- Team31Project/Product_List/models.py
+++ Team31Project/Product_List/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+    quantity_in_stock = models.IntegerField()
+    reorder_point = models.IntegerField()
+    active = models.BooleanField(default=True)
+    type = models.CharField(max_length=50)
+
+    class Meta:
+        managed = False
+        db_table = 'Stock'
--- Team31Project/Product_List/views.py
+++ Team31Project/Product_List/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
     return render(request, 'Product_List/Product_List.html', {'app_name': 'Product_List'})
+
+def index(request):
+    products = Product.objects.filter(active=True)
+    return render(request, 'Product_List/Product_List.html', {'products': products})
