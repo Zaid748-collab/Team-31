dd18931ab973bce49c9797c68e9a16d403f04ade
Harvey <240326227@aston.ac.uk>
2025-10-21 14:18:45 +0100

    Adding a navigation bar to base.html

--- Team31Project/Product_List/views.py
+++ Team31Project/Product_List/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'Profuct_List/Product_List.html', {'app_name': 'Product_List'})
+    return render(request, 'Product_List/Product_List.html', {'app_name': 'Product_List'})
