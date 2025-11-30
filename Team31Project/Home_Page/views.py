0aca02d1490c9b130f32c0d3910755ec80297eba
izjoshee <joshuailprimo@gmail.com>
2025-11-30 01:08:07 +0000

    Add responsive navbar and CTA buttons

--- Team31Project/Home_Page/views.py
+++ Team31Project/Home_Page/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from Product_List.models import Product
+from django.db.models import Max
 
 def home(request):
-    return render(request, "Home_Page/Home_Page.html")
+
+    # 1) Build dynamic categories WITH image + name
+    categories = (
+        Product.objects
+        .values('type')
+        .annotate(image_url=Max('image_url'))  # get 1 image per category
+        .order_by('type')
+    )
+
+    # 2) Featured = highest-priced, active products
+    featured_products = Product.objects.filter(active=True).order_by('-price')[:6]
+
+    return render(request, "Home_Page/Home_Page.html", {
+        "categories": categories,
+        "featured_products": featured_products,
+    })
