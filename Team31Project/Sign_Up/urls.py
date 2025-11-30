2b52ec70c2cc3ffb5c7eb7d92110aa3fd374db33
Zaid748-collab <zaidaj319@gmail.com>
2025-11-30 15:00:11 +0000

    fixing database error

--- Team31Project/Sign_Up/urls.py
+++ Team31Project/Sign_Up/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+<<<<<<< Updated upstream
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="Sign_Up/signup.html"), name="signup"),
+=======
+from .views import signup_view
+
+urlpatterns = [
+    path('', signup_view, name='signup'),
+>>>>>>> Stashed changes
 ]
