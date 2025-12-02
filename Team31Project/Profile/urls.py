40c964669c6811a23e83bb8f5bf2d18fa53af3e0
Harvey <240326227@aston.ac.uk>
2025-12-02 00:01:39 +0000

    Added Login functionality, superuser functionality,  expanded on profile/user pages and cleaned up the navigation bar

--- Team31Project/Profile/urls.py
+++ Team31Project/Profile/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.profile_view, name='profile'),
-    path('edit/', views.edit_profile, name='edit_profile'),
+    path('edit/', views.edit_profile, name='profile_edit'),
+
+    path("password/", 
+         auth_views.PasswordChangeView.as_view(template_name="Profile/change_password.html"), 
+         name="password_change"),
+    path("password/done/", 
+         auth_views.PasswordChangeDoneView.as_view(template_name="Profile/change_password_done.html"), 
+         name="password_change_done"),
+
 ]
--- Team31Project/Team31Project/urls.py
+++ Team31Project/Team31Project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,6 @@ urlpatterns = [
     path('orders/', include('Previous_Orders.urls')),
     path('checkout/', include('Checkout.urls')),
     path('profile/', include('Profile.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='Sign_Up/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
